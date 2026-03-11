693c11a18c3d72e3a1f22fe4323c39265ea77543
Samidha Chavan <samidhachavan05@gmail.com>
2026-03-11 10:13:09 +0530

    javascript

---
+++ javascript/programOne.js
@@ -0,0 +1,4 @@
+console.log('Hello World')
+console.log(2+4)
+
+alert('Hello From Alert')
---
+++ javascript/starPattern.js
@@ -0,0 +1,161 @@
+function printSquare(n){
+for (let i = 1; i <= n; i++) {
+    let row = '';
+    for (let j = 1; j <= n; j++) {
+        row += '*  ';
+    }
+    console.log(row);
+}
+
+}
+console.log('printSquare')
+printSquare(5)
+
+
+
+
+
+function printTriangle(n){
+
+for (let i = 1; i <= n; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '* ';
+    }
+    console.log(row);
+}
+}
+console.log('printTriangle')
+printTriangle(5)
+
+
+
+function printInvertedTriangle(n){
+for (let i = n; i >= 1; i--) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '* ';
+    }
+    console.log(row);
+}
+}
+console.log('printInvertedTriangle')
+printInvertedTriangle(5)
+
+
+
+function printPyramid(n){
+for (let i = 1; i <= n; i++) {
+    let row = '';
+
+    
+    for (let j = 1; j <= n - i; j++) {
+        row += '  ';
+    }
+
+    
+    for (let k = 1; k <= (2 * i - 1); k++) {
+        row += '* ';
+    }
+
+    console.log(row);
+}
+}
+
+console.log('printPyramid')
+printPyramid(5)
+
+
+
+function printDiamond(n){
+
+// upper pyramid
+for (let i = 1; i <= n; i++) {
+    let row = '';
+
+    for (let j = 1; j <= n - i; j++) {
+        row += '  ';
+    }
+
+    for (let k = 1; k <= (2 * i - 1); k++) {
+        row += '* ';
+    }
+
+    console.log(row);
+}
+
+// lower inverted pyramid
+for (let i = n - 1; i >= 1; i--) {
+    let row = '';
+
+    for (let j = 1; j <= n - i; j++) {
+        row += '  ';
+    }
+
+    for (let k = 1; k <= (2 * i - 1); k++) {
+        row += '* ';
+    }
+
+    console.log(row);
+}
+
+}
+
+console.log('printDiamond')
+printDiamond(5)
+
+
+
+function printNumberPyramid(n){
+for (let i = 1; i <= n; i++) {
+    let row = '';
+
+    for (let j = 1; j <= i; j++) {
+        row += i;
+    }
+
+    console.log(row);
+}
+}
+
+console.log('printNumberPyramid')
+printNumberPyramid(4)
+
+
+function printPascalTriangle(n){
+for (let i = 0; i < n; i++) {
+    let row = '';
+    let num = 1;
+
+    for (let j = 0; j <= i; j++) {
+        row += num + ' ';
+        num = num * (i - j) / (j + 1);
+    }
+
+    console.log(row);
+}
+}
+
+console.log('printPascalTriangle')
+printPascalTriangle(5)
+
+
+
+function printHollowSquare(n){
+for (let i = 1; i <= n; i++) {
+    let row = '';
+
+    for (let j = 1; j <= n; j++) {
+        if (i === 1 || i === n || j === 1 || j === n) {
+            row += '*';
+        } else {
+            row += ' ';
+        }
+    }
+
+    console.log(row);
+}
+}
+
+console.log('printHollowSquare')
+printHollowSquare(5)
